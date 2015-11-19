685d8ec50a02382d1334a6172d97c72135effc4e
Maciej Górski <maciek.gorski@gmail.com>
2015-11-20 02:41:49 +0300

    Add Crashlytics.

--- app/src/main/java/pl/mg6/cdwithease/MainActivity.java
+++ app/src/main/java/pl/mg6/cdwithease/MainActivity.java
@@ -18,6 +18,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.TextView;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
